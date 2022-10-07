b24def4b865e3f84eeff6ee5dd895535b46f9af9
igopereira1 <igopereira1@gmail.com>
2022-10-07 18:18:32 -0300

    feat req 04 g20 -> Testes

---
+++ src/tests/01.test.js
@@ -0,0 +1,33 @@
+import App from "../App";
+import React from "react";
+import Config from "../pages/Config";
+import Game from "../pages/Game";
+import Login from "../pages/Login";
+import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+import { cleanup, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+
+describe("Testando telas iniciais.", () => {
+    beforeEach(cleanup);
+    const mockToken = {
+        "response_code": 0,
+        "response_message": "Token Generated Successfully!",
+        "token": "f00cb469ce38726ee00a7c6836761b0a4fb808181a125dcde6d50a9f3c9127b6"
+    }
+    test('Teste', async () => {
+        renderWithRouterAndRedux(<App />);
+        const mockAPICall = jest.spyOn(window, 'fetch').mockReturnValue({
+        json: jest.fn().mockResolvedValue(mockToken),
+        });
+        userEvent.type(screen.getByTestId('input-player-name'), 'teste');
+        userEvent.type(screen.getByTestId('input-gravatar-email'), 'teste@teste.com');
+        waitFor(() => userEvent.click(screen.getByTestId('btn-play')));
+        expect(mockAPICall).toBeCalledTimes(1);
+        }); 
+    test("Testando botão de configuração.", () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+        userEvent.click(screen.getByTestId('btn-settings'));
+        expect(history.location.pathname).toBe("/config");
+    })  
+});
