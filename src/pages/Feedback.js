4cf2b9cbed6d9def2497651688b812bf7593c38a
Alysson Tobias <alyssonfontes8@gmail.com>
2022-10-11 12:11:07 -0300

    feat: main-group-20 -> requisito 13, que era pra ser o 12.

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Feedback extends React.Component {
+  state = {
+    wellDone: false,
+  };
+
+  componentDidMount() {
+    const { player: { assertions } } = this.props;
+    const THREE = 3;
+    if (assertions >= THREE) {
+      this.setState({ wellDone: true });
+    }
+  }
   render() {
+    const { wellDone } = this.state;
     return (
-      <h5 data-testid="feedback-text">Feedback text</h5>
+      <h5 data-testid="feedback-text">{wellDone ? 'Well Done!' : 'Could be better...'}</h5>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  player: state.player,
+});
+
+Feedback.propTypes = {
+  player: PropTypes.object,
+}.isRequired;
+
+export default connect(mapStateToProps)(Feedback);
